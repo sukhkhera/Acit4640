c32552ee1a52e77beeede854400503ac940ffd2d
sukhkhera <sukh.khera@hotmail.com>
2020-02-20 16:59:04 -0800

    fixed some issues

--- midterm/section_2/app_setup.sh
+++ midterm/section_2/app_setup.sh
@@ -1,41 +1,47 @@
 #!/bin/bash
 HICHAT_USER="hichat"
 NGINX_FILE="/home/$HICHAT_USER/nginx.conf"
+NGINX_DIR="/etc/nginx/"
 HOME_FOLDER="/app"
 PASSWORD="disabled"
 
 create_users() {
-    ssh -i midterm_id_rsa -p 12922 \
-        -o ConnectTimeout=2 -o StrictHostKeyChecking=no \
-        -q midterm exit "sudo useradd -m -d $HOME_FOLDER $HICHAT_USER -p $PASSWORD;"
+    ssh midterm \
+        "sudo useradd -p $(openssl passwd -1 disabled) -d $HOME_FOLDER hichat;"
 }
 
 
 install() {
-    ssh -i midterm_id_rsa -p 12922 \
-        -o ConnectTimeout=2 -o StrictHostKeyChecking=no \
-        -q midterm "sudo yum -y install git; \
-                    sudo yum -y install npm; \
-                    sudo yum -y install nodejs; \
-                    sudo yum -y install nginx;"
+    ssh midterm \
+        "sudo yum -y install git; \
+        sudo yum -y install npm; \
+        sudo yum -y install nodejs; \
+        sudo yum -y install nginx; \
+        "
+}
+
+copy_files(){
+    ssh midterm \
+        "sudo cp -f $NGINX_FILE  $NGINX_DIR;"
 }
 
 git_clone() {
-    ssh -i midterm_id_rsa -p 12922 \
-        -o ConnectTimeout=2 -o StrictHostKeyChecking=no \
-        -q midterm "sudo chmod 777 $HOME_FOLDER; \
-        cd $HOME_FOLDER; \
+    ssh midterm \
+        "sudo chmod 777 $HOME_FOLDER; \
         sudo chown $HICHAT_USER $HOME_FOLDER; \
+        cd $HOME_FOLDER; \
         git clone https://github.com/wayou/HiChat /$HOME_FOLDER; \
-        cd /app; \
-        npm install;"
+        cd $HOME_FOLDER; \
+        npm install; \
+        "
 }
 
 start_services(){
     sudo systemctl enable nginx
-    sudo systemctl daemon-reload
     sudo systemctl enable hichat
-
+    sudo systemctl daemon-reload
+    sudo systemctl start nginx
+    sudo systemctl start hichat
 }
 
 server_start() {
